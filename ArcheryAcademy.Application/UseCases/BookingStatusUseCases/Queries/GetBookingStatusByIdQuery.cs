edaedf053fa067f2fc2fd66bd4b6518af9c830d4
jeefry.zamata@tecsup.edu.pe <jeefry.zamata@tecsup.edu.pe>
2025-11-27 01:11:01 -0500

    feat: Implementation UseCase Query for GetById

---
+++ ArcheryAcademy.Application/UseCases/BookingStatusUseCases/Queries/GetBookingStatusByIdQuery.cs
@@ -0,0 +1,19 @@
+using ArcheryAcademy.Application.DTOs.BookingStatusDto;
+using ArcheryAcademy.Domain.Entities;
+using ArcheryAcademy.Domain.Ports;
+using AutoMapper;
+using MediatR;
+
+namespace ArcheryAcademy.Application.UseCases.BookingStatusUseCases.Queries;
+
+public record GetBookingStatusByIdQuery(int Id) : IRequest<BookingStatusReadDto?>;
+
+internal sealed class GetBookingStatusByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    : IRequestHandler<GetBookingStatusByIdQuery, BookingStatusReadDto?>
+{
+    public async Task<BookingStatusReadDto?> Handle(GetBookingStatusByIdQuery request, CancellationToken cancellationToken)
+    {
+        var bookingStatus = await unitOfWork.Repository<BookingStatus>().GetByIdAsync(request.Id);
+        return bookingStatus == null ? null : mapper.Map<BookingStatusReadDto>(bookingStatus);
+    }
+}
