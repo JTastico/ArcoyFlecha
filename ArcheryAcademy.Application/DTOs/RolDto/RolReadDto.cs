27436780b6bac74cb94f8e27b1696a6ea9ca5239
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2025-12-03 12:35:29 -0500

    feat(dto): add RoleId field to UserReadDto and related DTOs

--- ArcheryAcademy.Application/DTOs/RolDto/RolReadDto.cs
+++ ArcheryAcademy.Application/DTOs/RolDto/RolReadDto.cs
@@ -2,7 +2,7 @@ namespace ArcheryAcademy.Application.DTOs.RolDto;
 
 public class RolReadDto
 {
-
+    public Guid Id { get; set; }
     public string Name { get; set; } = null!;
 
     public string? Description { get; set; }
